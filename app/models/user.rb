65aaa7b9a8ab4fb3e76daf441ea8bfe3967254b7
Francisco Chavarria <fchavarria@infile.com>
2024-10-02 10:11:29 -0600

    🦺 Validate
    change: create form validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, presence: true, length: { minimum: 8, maximum: 128 }, password: true
 end
