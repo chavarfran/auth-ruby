607e0403b2dac0d3154f091bbc68e1e1ec5ac684
Francisco Chavarria <fchavarria@infile.com>
2024-10-03 11:28:28 -0600

    🔧 Config: edit devise security

--- config/initializers/devise_security.rb
+++ config/initializers/devise_security.rb
@@ -42,7 +42,7 @@ Devise.setup do |config|
   # config.captcha_for_confirmation = true
 
   # Time period for account expiry from last_activity_at
-  # config.expire_after = 90.days
+  config.expire_after = 2.days
 
   # Allow password to equal the email
   # config.allow_passwords_equal_to_email = false
--- db/migrate/20240927164055_devise_create_users.rb
+++ db/migrate/20240927164055_devise_create_users.rb
@@ -24,7 +24,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
+      t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
@@ -38,6 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :password_changed_at
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
